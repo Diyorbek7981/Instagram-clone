1aa714136dbba342381a58c81f5690b2fa9dca9c
Diyorbek7981 <abdurahimov.diyorbek7981@gmail.com>
2024-10-11 21:53:51 +0500

    isdfaf

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
+    path('list/', PostListView.as_view(), name='post-list'),
     path('retrive_update/<int:pk>', PostRetrieveUpdateDestroyAPIView.as_view(), name='post-retrieve-update-destroy'),
     path('coment_create/<int:pk>', PostCommentCreateAPIView.as_view(), name='post-comment-create'),
     path('coment_list/<int:pk>', PostCommentListAPIView.as_view()),
